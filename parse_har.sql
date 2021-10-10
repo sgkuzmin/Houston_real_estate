af5250fdd0659fb79535dcc6b59b8a905511470b
wycislo <wycislo@yahoo.com>
2021-10-10 10:02:33 -0500

    update address

--- parse_har.sql
+++ parse_har.sql
@@ -38,17 +38,25 @@ add column city text;
 
 
 select position(' ' in address ) as "start", position(',' in address) - position(' ' in address ) as "length" from har;
-
-
 select substring(address, position(' ' in address),position(',' in address) - position(' ' in address )) as street_name from har
-
+select split_part(address,',',2)  as city from har;
 
 select length(address) from har;
-
 select positon(',' in address ) from har;
 select positon(' ' address ) from har;
 
 drop table har2;
 create table har2 as select mls, cast(substring(address,1,position(' ' in address)-1) as integer) as str_num ,
-substring(address, position(' ' in address),position(',' in address) - position(' ' in address )) as street_name from har;
+substring(address, position(' ' in address),position(',' in address) - position(' ' in address )) as street_name,
+split_part(address, ',',2) as city from har;
 select * from har2;
+
+select * from har;
+update har
+set house_number = har2.str_num,
+str_sfx=har2.street_name,
+city=har2.city
+from har2
+where har.mls = har2.mls
+
+
